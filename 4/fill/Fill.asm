b11aaaffb91f061ab1f966e5e3a9f5eb25fffc9b
Calf <calf.zhou@gmail.com>
2026-05-23 18:45:40 +0800

    feat(project4): implement Mult and Fill

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -2,9 +2,80 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+/*
+    previous = -1
+LOOP:
+    if (kbd == previous) goto LOOP
+    previous = kbd
+    color = white
+    if (previous == 0) goto FILL
+    color = black
+FILL:
+    pos = SCREEN + 8k
+LOOP2:
+    pos = pos - 1
+    *pos = color
+    if (pos == SCREEN) goto LOOP
+    goto LOOP2
+*/
+
+// previous = -1
+@previous
+M=-1
+(LOOP)
+// if (kbd == previous) goto LOOP
+@KBD
+D=M
+@previous
+D=D-M
+@LOOP
+D;JEQ
+// previous = kbd
+@KBD
+D=M
+@previous
+M=D
+// color = white
+@color
+M=0
+// if (previous == 0) goto FILL
+@previous
+D=M
+@FILL
+D;JEQ
+// color = black
+@color
+M=!M
+(FILL)
+// pos = SCREEN + 8k
+@SCREEN
+D=A
+@8192
+D=D+A
+@pos
+M=D
+(LOOP2)
+// pos = pos - 1
+@pos
+M=M-1
+// *pos = color
+@color
+D=M
+@pos
+A=M
+M=D
+// if (pos == SCREEN) goto LOOP
+@pos
+D=M
+@SCREEN
+D=D-A
+@LOOP
+D;JEQ
+// goto LOOP2
+@LOOP2
+0;JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -6,4 +6,65 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Bitwise multiplication.
+/*
+    R2 = 0
+    mask = 1
+LOOP:
+    if (mask == 0) goto END
+    if (R1 & mask == 0) goto SKIP
+    R2 = R2 + R0
+SKIP:
+    mask = 2 * mask
+    R0 = 2 * R0
+    goto LOOP
+END:
+*/
+
+// R2 = 0
+@R2
+M=0
+// mask = 1
+@mask
+M=1
+(LOOP)
+// if (mask == 0) goto END
+@mask
+D=M
+@END
+D;JEQ
+// if (R1 & mask == 0) goto SKIP
+@R1
+D=M
+@mask
+D=D&M
+@SKIP
+D;JEQ
+// R2 = R2 + R0
+@R2
+D=M
+@R0
+D=D+M
+@R2
+M=D
+(SKIP)
+// mask = 2 * mask
+@mask
+D=M
+@mask
+D=D+M
+@mask
+M=D
+// R0 = 2 * R0
+@R0
+D=M
+@R0
+D=D+M
+@R0
+M=D
+// goto LOOP
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
